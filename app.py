e42dd5abd1cd51e8132ab23df36aa0f754c0738a
prince98908-arch <prince98908@gmail.com>
2026-01-02 17:47:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ if st.button("Predict Insurance Cost"):
     # Prediction result
     prediction = model.predict(input_data)
     
-    st.success(f"### 💵 Estimated Charges: ${prediction[0]:,.2f}")
+    st.success(f"### ₹ Estimated Charges: ${prediction[0]:,.2f}")
